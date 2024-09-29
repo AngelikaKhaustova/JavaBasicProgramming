c3a16cbb6f9a57e2e0c397117890e2682600e3a5
Ang <zajkopus@gmail.com>
2024-09-29 15:09:57 +0200

    Homework14_Task2

---
+++ src/homework_14/homework_14.java
@@ -0,0 +1,21 @@
+package homework_14;
+
+public class homework_14 {
+    public static void main(String[] args) {
+        int[] massiv = new int[]{1, 2, 3, 4, 8, 10, 12, 13, 15, 20};
+        int summa;
+        summa = sumOdd(massiv);
+        System.out.println("Сумма четных чисел= " + summa);
+
+    }
+
+    public static int sumOdd(int[] massiv) {
+        int res = 0;
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i] % 2 == 0) {
+                res = res + massiv[i];
+            }
+        }
+        return res;
+    }
+}

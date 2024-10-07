ce4bfccdd99bf8284c8cf443aa1215bb2eb9be20
Ang <zajkopus@gmail.com>
2024-10-07 20:36:14 +0200

    Homework 18 updated code format

--- src/homwork_18/Calculator.java
+++ src/homwork_18/Calculator.java
@@ -1,16 +1,19 @@
 package homwork_18;
 
 public class Calculator {
-    public static double addition(double x, double y){
-        return x+y;
+    public static double addition(double x, double y) {
+        return x + y;
     }
-    public static double subtraction(double x, double y){
-        return x-y;
+
+    public static double subtraction(double x, double y) {
+        return x - y;
     }
-    public static double multiplication (double x, double y){
-        return x*y;
+
+    public static double multiplication(double x, double y) {
+        return x * y;
     }
-    public static double division (double x, double y){
-        return x/y;
+
+    public static double division(double x, double y) {
+        return x / y;
     }
 }
--- src/homwork_18/CalculatorApp.java
+++ src/homwork_18/CalculatorApp.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class CalculatorApp {
     public static void main(String[] args) {
         System.out.println("Сумма двух чисел 1+1=" + Calculator.addition(1, 1));
-        System.out.println("Разница двух чисел 17-7="+ Calculator.subtraction(17,7 ));
-        System.out.println("Результат умножения двух чисел 3*5="+ Calculator.multiplication(3,5));
-        System.out.println("Результат деления двух чисел 81/9="+Calculator.division(81,9));
+        System.out.println("Разница двух чисел 17-7=" + Calculator.subtraction(17, 7));
+        System.out.println("Результат умножения двух чисел 3*5=" + Calculator.multiplication(3, 5));
+        System.out.println("Результат деления двух чисел 81/9=" + Calculator.division(81, 9));
     }
 
 }

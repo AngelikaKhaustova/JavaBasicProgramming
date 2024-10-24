31f9c58404434646e7127e94aee24a27d39dcfd8
Ang <zajkopus@gmail.com>
2024-10-24 20:09:16 +0200

    Homework 31

---
+++ src/homework_31/Seasons.java
@@ -0,0 +1,25 @@
+package homework_31;
+
+public enum Seasons {
+    WINTER("Зима", -2.2),
+    SPRING("Весна", 10.5),
+    SUMMER("Лето", 25.7),
+    AUTUMN("Весна", 8.0);
+
+
+    private String russian;
+    private double avgTemp;
+
+    Seasons(String russian, double avgTemp) {
+        this.avgTemp = avgTemp;
+        this.russian = russian;
+    }
+
+    public String getRussian() {
+        return russian;
+    }
+
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+}
---
+++ src/homework_31/SeasonsTest.java
@@ -0,0 +1,9 @@
+package homework_31;
+
+public class SeasonsTest {
+    public static void main(String[] args) {
+        for (Seasons seasons : Seasons.values()) {
+            System.out.println(seasons.getRussian() + " средняя температура: "+ seasons.getAvgTemp());
+        }
+    }
+}
